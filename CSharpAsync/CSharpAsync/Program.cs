85d0f0c2d956d56cff0219b8e6992450e778a2a8
William Lim <manvinedi@gmail.com>
2020-02-04 00:10:03 +0800

    added first async example

--- CSharpAsync/CSharpAsync/Program.cs
+++ CSharpAsync/CSharpAsync/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace CSharpAsync {
     class Program {
-        static void Main (string[] args) {
-            Console.WriteLine("Hello World!");
+        private static async Task Main (string[] args) {
+            string contents = await ReadTestFile();
+            Console.WriteLine(contents);
+        }
+
+        public static async Task<string> ReadTestFile () {
+            string fileData = await File.ReadAllTextAsync("../../../Test.txt");
+            return fileData;
         }
     }
 }
